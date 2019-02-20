5b341097dc6a270ba759d85e074ed61c17f9cec6
Salman <rahman_5@live.ca>
2019-02-19 23:59:27 -0500

    Android Provided BottomBar With Material Theme

---
+++ app/src/main/java/ca/engrLabs_390/engrlabs/ExpandableRecyclerWithBottomNav.java
@@ -0,0 +1,50 @@
+package ca.engrLabs_390.engrlabs;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ExpandableRecyclerWithBottomNav extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_floor_8:
+                    mTextMessage.setText(R.string._8);
+                    return true;
+                case R.id.navigation_floor_9:
+                    mTextMessage.setText(R.string._9);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expandable_recycler_with_bottom_nav);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/ca/engrLabs_390/engrlabs/MainActivity.java
+++ app/src/main/java/ca/engrLabs_390/engrlabs/MainActivity.java
@@ -1,6 +1,7 @@
 // This is the barebone Project
 package ca.engrLabs_390.engrlabs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // call goto_expandableRecyclerWithBottomNav() to go to expandableRecycler with Bottom Nav
+        goto_expandableRecyclerWithBottomNav();
+
+    }
+
+
+    // going from mainActivity to ExpandableRecylerView with Bottom Nav
+    public void goto_expandableRecyclerWithBottomNav() {
+        // this intent object allows to link activity1 to activity2
+        // Intent intent = new Intent(MainActivityView.this, second_activity.class);
+        Intent intent = new Intent(this, ExpandableRecyclerWithBottomNav.class);
+
+        // put extra is used to primitive data from one activity to another
+        //        intent.putExtra("courseID", courseID);
+
+        startActivity(intent);
     }
 }

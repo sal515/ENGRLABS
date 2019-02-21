a0b0a1ec0857fe3b2ca7e4a240515fdbd85553ac
Salman <rahman_5@live.ca>
2019-02-20 20:01:21 -0500

    Going to set visible and gone properties for layout to make the accordion rather than actual accordion

--- app/src/main/java/ca/engrLabs_390/engrlabs/recyclerView/dataAdapter_recyclerView.java
+++ app/src/main/java/ca/engrLabs_390/engrlabs/recyclerView/dataAdapter_recyclerView.java
@@ -25,31 +25,6 @@ import ca.engrLabs_390.engrlabs.database_files.recyclerViewData;
 // Note that we specify the custom ViewHolder which gives us access to our views
 public class dataAdapter_recyclerView extends ListAdapter<recyclerViewData, dataAdapter_recyclerView.ViewHolder> {
 
-    //==================== Define View Holder =============================
-    // Provide a direct reference to each of the views within a data item
-    // Used to cache the views within the item layout for fast access
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
-        // Your holder should contain a member variable
-        // for any view that will be set as you render a row
-
-        public TextView textViewData1;
-        public TextView textViewData2;
-
-        // We also create a constructor that accepts the entire item row
-        // and does the view lookups to find each subview
-        public ViewHolder(View itemView) {
-            // Stores the itemView in a public final member variable that can be used
-            // to access the context from any ViewHolder instance.
-            super(itemView);
-
-            textViewData1 = itemView.findViewById(R.id.rowTextView1);
-            textViewData2 = itemView.findViewById(R.id.rowTextView2);
-        }
-    }
-
-
-    //================================================================
 
     //==================== Fill Adapter with Data Model =============================
 
@@ -80,6 +55,34 @@ public class dataAdapter_recyclerView extends ListAdapter<recyclerViewData, data
             };
 
     //==================== ============================= =============================
+
+
+    //==================== Define View Holder =============================
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+
+        public TextView textViewData1;
+        public TextView textViewData2;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            textViewData1 = itemView.findViewById(R.id.rowTextView1);
+            textViewData2 = itemView.findViewById(R.id.rowTextView2);
+        }
+    }
+
+
+    //================================================================
+
 
     //==================== 3 Primary Methods of RecyclerView =============================
 

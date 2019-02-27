60a6d13fb152d3ca668b63f9202f6b346bc611c3
Mike Yabz <mikeyabz19@gmail.com>
2019-02-26 23:41:08 -0500

    Working Search Bar suggestions

--- app/src/main/java/ca/engrLabs_390/engrlabs/ExpandableRecyclerWithBottomNav.java
+++ app/src/main/java/ca/engrLabs_390/engrlabs/ExpandableRecyclerWithBottomNav.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import ca.engrLabs_390.engrlabs.Parser.Parser;
+import ca.engrLabs_390.engrlabs.Parser.Software;
 import ca.engrLabs_390.engrlabs.database_files.recyclerViewData;
 import ca.engrLabs_390.engrlabs.recyclerView.dataAdapter_recyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -21,6 +26,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Vector;
 
 public class ExpandableRecyclerWithBottomNav extends AppCompatActivity {
 
@@ -32,6 +38,13 @@ public class ExpandableRecyclerWithBottomNav extends AppCompatActivity {
 
     // Recycler Adapter variable declaration
     dataAdapter_recyclerView recyclerViewAdapter;
+
+    //Suggestion list for search bar
+    List<String> suggestList = new ArrayList<>();
+    List<String> fullSuggestList = new ArrayList<>();   //will contain the entire list of softwares
+
+    //Search Bar
+    MaterialSearchBar materialSearchBar;
 
     // Why and How recyclerView: https://guides.codepath.com/android/using-the-recyclerview
     // Using a RecyclerView has the following key steps:
@@ -67,6 +80,52 @@ public class ExpandableRecyclerWithBottomNav extends AppCompatActivity {
 
         // Initialize RecyclerView variable
         recyclerViewVar = findViewById(R.id.expandingRecyclerView);
+
+        //Initialize Search Bar and SUggestion List
+        populateSuggestionList();
+        materialSearchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+        materialSearchBar.setHint("Enter your software or 'all'");
+        materialSearchBar.setVisibility(View.GONE);
+        materialSearchBar.setLastSuggestions(suggestList);
+        materialSearchBar.setCardViewElevation(10);
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //When user type their text, the suggestion list will update
+                updateSuggestList(materialSearchBar.getText());
+                materialSearchBar.updateLastSuggestions(suggestList);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+    }
+
+    private void updateSuggestList(String inputString){
+        suggestList.clear();
+        if (inputString.length()<3) {    //if no text was entered, no substring searching needed
+            materialSearchBar.clearSuggestions();
+            return;
+        }
+        suggestList.addAll(fullSuggestList);    //all should print out everything as a suggestion
+        if (inputString.toLowerCase().equals("all")){
+            return;
+        }
+        for(int i =0;i<suggestList.size();i++){ //iterate through suggestList
+            String temp = suggestList.get(i).toLowerCase();
+            if (!temp.contains(inputString.toLowerCase())){
+                suggestList.remove(i);   //remove anything that isn't a match
+                i--;
+            }
+        }
+        if (suggestList.size() == 0){   //if nothings left that means there were no matches, just output No Matches
+            suggestList.add("No Results");
+        }
     }
 
     private void setListeneres() {
@@ -89,7 +148,13 @@ public class ExpandableRecyclerWithBottomNav extends AppCompatActivity {
                     mTextSeletectionTextBox.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextSeletectionTextBox.setText(R.string.title_notifications);
+                    if(materialSearchBar.getVisibility() == View.VISIBLE)
+                    {
+                        materialSearchBar.setVisibility(View.GONE);
+                    }
+                    else{
+                        materialSearchBar.setVisibility(View.VISIBLE);
+                    }
                     return true;
                 case R.id.navigation_floor_8:
                     mTextSeletectionTextBox.setText(R.string.Floor_8);
@@ -136,6 +201,25 @@ public class ExpandableRecyclerWithBottomNav extends AppCompatActivity {
 //        itemAnimator.setRemoveDuration(1000);
 //        recyclerViewVar.setItemAnimator(itemAnimator);
 
+    }
+
+    private void populateSuggestionList(){
+        Vector<Software> soft;  //make list to store software
+        Parser parser = new Parser();
+        soft = parser.parse(this);  //parse the file in Assets folder
+        fullSuggestList.clear();    //erase current suggest list
+        for(int i =0;i<soft.size();i++){    //iterate through software list
+            String temp = soft.get(i).softwareName;
+            boolean duplicate = false;
+            for(int j =0;j<fullSuggestList.size();j++){ //checks for duplicates
+                if (fullSuggestList.get(j).equals(temp)){
+                    duplicate = true;
+                }
+            }
+            if (duplicate == false){    //if software wasnt already added, add it
+                fullSuggestList.add(temp);
+            }
+        }
     }
 
 

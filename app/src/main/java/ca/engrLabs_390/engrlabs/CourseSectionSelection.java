363cc52773ab04e3f9a74a7a12240a07a1ae1ce1
MikeYabz <mikeyabz19@gmail.com>
2019-03-05 19:43:43 -0500

    added dialog fragment for adding courses

--- app/src/main/java/ca/engrLabs_390/engrlabs/CourseSectionSelection.java
+++ app/src/main/java/ca/engrLabs_390/engrlabs/CourseSectionSelection.java
@@ -1,9 +1,13 @@
 package ca.engrLabs_390.engrlabs;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -19,9 +23,17 @@ public class CourseSectionSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_section_selection);
-        signOutButton = findViewById(R.id.signOutButton);
+
+        //List
         classList = findViewById(R.id.selectedList);
 
+        //format ActionBar and Content
+        ActionBar ab = getSupportActionBar();   //get the Action Bar object
+        ab.setTitle("My Grades");   //set the title
+        ab.setDisplayHomeAsUpEnabled(true); //enable UP button, parent is declared in the manifest
+
+        //Sign out Button
+        signOutButton = findViewById(R.id.signOutButton);
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,5 +42,28 @@ public class CourseSectionSelection extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    //Creates ActionBar Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_course_selection,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Handles action button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_course:
+                DialogFragment dialog = new DialogFragment();
+                dialog.show(getSupportFragmentManager(), "Insert Course");
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

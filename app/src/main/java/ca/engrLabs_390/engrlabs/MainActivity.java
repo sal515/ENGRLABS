843a7baf7f5852eea124e843f0f33e87047aabd7
Mike Yabz <mikeyabz19@gmail.com>
2019-04-01 22:52:25 -0400

    added scheduled notifications

--- app/src/main/java/ca/engrLabs_390/engrlabs/MainActivity.java
+++ app/src/main/java/ca/engrLabs_390/engrlabs/MainActivity.java
@@ -1,6 +1,9 @@
 // This is the barebone Project
 package ca.engrLabs_390.engrlabs;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Slide;
@@ -8,9 +11,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Calendar;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 import ca.engrLabs_390.engrlabs.TA_Section.LoginActivity;
 import ca.engrLabs_390.engrlabs.dataModels.SIngleton2ShareData;
+import ca.engrLabs_390.engrlabs.notifications.AlarmReceiver;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     Button loginBtn;
     Button homepageBtn;
     ImageView logo;
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         initializeReferences();
         initializeListeners();
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        setScheduledNotification(Calendar.MONDAY,22,45,0);  //Monday at 10:45pm
 
     }
 
@@ -80,5 +92,23 @@ public class MainActivity extends AppCompatActivity {
     public void goto_loginActivity() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void setScheduledNotification(int dayOfWeek, int hourOfDay, int minute, int second){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(this, AlarmReceiver.class);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(Calendar.DAY_OF_WEEK,dayOfWeek);
+        cal.set(Calendar.HOUR_OF_DAY,hourOfDay);
+        cal.set(Calendar.MINUTE,minute);
+        cal.set(Calendar.SECOND,second);
+
+        //cal.add(Calendar.MINUTE, 1);
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
     }
 }
